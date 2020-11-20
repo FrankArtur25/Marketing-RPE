9c7a9f933c552a22998c44c6c0ac819b317d048a
FrankArtur25 <69621680+FrankArtur25@users.noreply.github.com>
2020-11-20 17:29:05 -0500

    Add files via upload

--- form/send.php
+++ form/send.php
@@ -1,44 +1,26 @@
 
     <?php
 
-    if(isset($_POST['enviar'])) {
-        if (!empty($_POST['name']) && !empty($_POST['profesion']) && !empty($_POST['email']) && !empty($_POST['telf']) && !
-        empty($_POST['ciudad']) && !empty($_POST['msg'])) {
-            $name = $_POST['name'];
-            $profesion = $_POST['profesion'];
-            $email = $_POST['email'];
-            $telf = $_POST['telf'];
-            $ciudad = $_POST['ciudad'];
-            $msg = $_POST['msg'];
-            $header = "From: $email" . "\r\n";
-            $header = "Reply-To: $email" . "\r\n";
-            $header = "X-Mailer: PHP/" . phpversion();
-            $mail = @mail($email, $msg, $header);
-            if($mail) {
-                echo "<h4>!Mail enviado exitosamente!</h4>";
-            }
-        }
-    }
-    /*try {
+    try {
         $name = $_REQUEST["name"];
         $email = $_REQUEST["email"];
-        $city = $_REQUEST["ciudad"];
+        $city = $_REQUEST["city"];
         $mobile = $_REQUEST["telf"];
-        $profesion = $_REQUEST["profesion"];
-        $dni = $_REQUEST["identidad"];
+        $profession = $_REQUEST["profesion"];
+        $message_client = $_REQUEST["message"];
         $subject = "Necesito información";
 
         $message = "Nombre : ".$name."\n";
-        $message.= "Profesión : ".$profesion."\n";
+        $message.= "Profesión : ".$profession."\n";
         $message.= "Ciudad : ".$city."\n";
         $message.= "Correo : ".$email."\n";
         $message.= "Celular : ".$mobile."\n";
-        $message.= "Dato Importante: ".$data."\n";
-        $myemail = "areadesistemas.kreap@gmail.com";
+        $message .= "Mensaje : ".$message_client;
+        $myemail = "frank_artur_25@hotmail.com";
         $headers = "From: $email";
         mail($myemail, $subject, $message, $headers);
         echo true;
     } catch (Exception $e) {
         echo $e;
-    }*/
+    }
 ?>
